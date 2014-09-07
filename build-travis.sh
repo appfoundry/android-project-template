7f8de053dce89d39dfb6f7c683c52535ad63d0a8
Filip Maelbrancke <filip@maelbrancke.net>
2014-09-07 18:13:04 +0200

    Initial Travis CI config

---
+++ build-travis.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./gradlew :build
+#          :appfoundry-core:testDebug \
+#          :appfoundry-robo-tests:testDebug \
+#          :appfoundry-app:connectedAndroidTest \
+#          :appfoundry-robo-tests:jacocoTestReport \
+#          :appfoudnry-robo-tests:coveralls \
