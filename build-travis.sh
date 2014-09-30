6a02c87a9186afa89d34fa25e5f38f15cd19c153
Filip Maelbrancke <filip@maelbrancke.net>
2014-09-30 11:22:03 +0200

    Run unit tests @travis

--- build-travis.sh
+++ build-travis.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./gradlew :build :appfoundry-app:connectedAndroidTest
-          :appfoundry-core:test
-          :appfoundry-robo-tests:test
+./gradlew :build :appfoundry-app:connectedAndroidTest \
+          :appfoundry-core:test \
+          :appfoundry-robo-tests:test \
 #          :appfoundry-robo-tests:jacocoTestReport \
 #          :appfoundry-robo-tests:coveralls \
