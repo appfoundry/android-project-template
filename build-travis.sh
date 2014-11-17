0dd48eb2e3373b224ca08e7dfb60b5f31a4862ae
Filip Maelbrancke <filip@maelbrancke.net>
2014-11-17 17:53:09 +0100

    updated dependencies

--- build-travis.sh
+++ build-travis.sh
@@ -3,5 +3,5 @@
 ./gradlew :build :appfoundry-app:connectedAndroidTest \
           :appfoundry-core:test \
           :appfoundry-robo-tests:test \
-#          :appfoundry-robo-tests:jacocoTestReport \
+          :appfoundry-robo-tests:jacocoTestReport \
 #          :appfoundry-robo-tests:coveralls \
